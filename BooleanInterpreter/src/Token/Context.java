99b15874da36953791898fe942a89eaba74dbca9
Xavier <mawet.xavier@gmail.com>
2014-12-16 16:29:04 +0100

    Ajout de la classe Context Cette classe à pour rôle de contenir toutes les associations identifieur <-> valeur

---
+++ BooleanInterpreter/src/Token/Context.java
@@ -0,0 +1,31 @@
+package Token;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Nakim
+ */
+public class Context
+{
+    private HashMap<String, Boolean> values;
+
+    //<editor-fold defaultstate="collapsed" desc="Constructeur">
+    public Context()
+    {
+        this.values = new HashMap<>();
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Public methods">
+    public boolean setValue(String identifier, boolean value)
+    {
+        return this.values.put(identifier, value);
+    }
+    
+    public boolean getValue(String identifier)
+    {
+        return this.values.get(identifier);
+    }
+    //</editor-fold>
+}
