f75ce6345daf87eefbec5ace1d3430e3e08ba7a2
Xavier <mawet.xavier@gmail.com>
2014-12-17 15:00:39 +0100

    Utilisation du parser (Main.java)

--- BooleanInterpreter/src/Main/Main.java
+++ BooleanInterpreter/src/Main/Main.java
@@ -2,13 +2,13 @@ package Main;
 
 import Interpreter.Parser;
 import Interpreter.Context;
+import Symbols.NonTerminalSymbol;
+import Utils.ContextErrorException;
 import Utils.ParsingErrorException;
 import Utils.ReturnValue;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -20,23 +20,36 @@ public class Main
      * @param args the command line arguments
      */
     public static void main(String[] args)
-    {
-        // Create context (identifier - value)
-        Context context = new Context();
-        context.setValue("true", true);
-        context.setValue("false", false);
-        context.setValue("x", true);
-        context.setValue("y", false);
-        context.setValue("z", true);
-        
+    {        
         // Instancier le parseur en lui donnant la source de données
         String text = "(true and X) or (Y and Z)";
         InputStream stream = new ByteArrayInputStream(text.getBytes());
         Parser parser = new Parser(stream);
         
+        // Create context (identifier - value)
+        Context context = new Context();
+        context.setValue("true", true);
+        context.setValue("false", false);
+        context.setValue("x", false);
+        context.setValue("y", true);
+        context.setValue("z", true);
+        
         try
         {
-            parser.parse();
+            // "Build" tree
+            NonTerminalSymbol symbol = parser.parse();
+            
+            System.out.println("Parsing Ok...");
+            
+            // Interprets result with the context
+            boolean result = symbol.interpret(context);
+            System.out.println("Result : " + result);
+
+        }
+        catch (ContextErrorException ex)
+        {
+            System.err.println("Context error : " + ex.getMessage());
+            System.exit(ReturnValue.FAILURE.getReturnCode());
         }
         catch (ParsingErrorException ex)
         {
