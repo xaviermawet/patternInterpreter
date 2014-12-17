a621103b639294c21bc9a1ce2c8b7525290d55c7
Xavier <mawet.xavier@gmail.com>
2014-12-17 16:29:12 +0100

    Projet MathematicsInterpreter initial

---
+++ MathematicsInterpreter/src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+/**
+ *
+ * @author Nakim
+ */
+public class Main
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+    }
+}
