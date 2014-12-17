10c0fab787d12245cad62df38f28886282c9bb2a
Xavier <mawet.xavier@gmail.com>
2014-12-17 22:33:30 +0100

    Math : Ajout de la classe Facteur

--- MathematicsInterpreter/src/Symbols/Facteur.java
+++ MathematicsInterpreter/src/Symbols/Facteur.java
@@ -9,9 +9,22 @@ import Utils.ContextErrorException;
  */
 public class Facteur implements Symbol
 {
+    private final Symbol atome;
+    private final Symbol facteur;
+
+    public Facteur(Symbol atome, Symbol facteur)
+    {
+        this.atome = atome;
+        this.facteur = facteur;
+    }
+    
     @Override
     public double interpret(Context context) throws ContextErrorException
     {
-        return 0;
+        if (this.facteur == null)
+            return this.atome.interpret(context);
+        
+        return Math.pow(this.atome.interpret(context),
+                        this.facteur.interpret(context));
     }
 }
