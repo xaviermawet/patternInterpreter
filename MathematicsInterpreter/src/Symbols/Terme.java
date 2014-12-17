f77fbf04c00491edbea81436733c4870ea0e2c74
Xavier <mawet.xavier@gmail.com>
2014-12-17 22:17:17 +0100

    Math : Ajout de la classe Terme

--- MathematicsInterpreter/src/Symbols/Terme.java
+++ MathematicsInterpreter/src/Symbols/Terme.java
@@ -9,9 +9,41 @@ import Utils.ContextErrorException;
  */
 public class Terme implements Symbol
 {
+    public static final boolean MULTIPLICATION;
+    public static final boolean DIVISION;
+    
+    private final Symbol  facteur;
+    private final boolean multiplication;
+    private final Symbol  terme;
+    
+    static
+    {
+        MULTIPLICATION = true;
+        DIVISION = false;
+    }
+
+    public Terme(Symbol facteur, boolean multiplication, Symbol terme)
+    {
+        this.facteur = facteur;
+        this.multiplication = multiplication;
+        this.terme = terme;
+    }
+
+    public Terme(Symbol facteur)
+    {
+        this.facteur = facteur;
+        this.multiplication = false;
+        this.terme = null;
+    }
+
     @Override
     public double interpret(Context context) throws ContextErrorException
     {
-        return 0;
+        if (this.terme == null)
+            return this.facteur.interpret(context);
+        
+        return (this.multiplication) ?
+               this.facteur.interpret(context) * this.terme.interpret(context) :
+               this.facteur.interpret(context) / this.terme.interpret(context);
     }
 }
