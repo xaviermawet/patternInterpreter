d50841f590c6c138c59a705f3a9489bd3452ba4c
Xavier <mawet.xavier@gmail.com>
2014-12-18 20:09:33 +0100

    Math : Ajout de la classe TrigoAtome

---
+++ MathematicsInterpreter/src/Symbols/TrigoAtome.java
@@ -0,0 +1,40 @@
+package Symbols;
+
+import Utils.Context;
+import Utils.ContextErrorException;
+
+/**
+ *
+ * @author Nakim
+ */
+public class TrigoAtome implements Symbol
+{
+    public static final int SIN = 1;
+    public static final int COS = 2;
+    public static final int TAN = 3;
+    
+    private final Symbol atome;
+    private final int trigonometricFunction;
+
+    public TrigoAtome(Symbol atome, int trigonometricFunction)
+    {
+        this.atome = atome;
+        this.trigonometricFunction = trigonometricFunction;
+    }
+
+    @Override
+    public double interpret(Context context) throws ContextErrorException
+    {
+        switch(this.trigonometricFunction)
+        {
+            case SIN:
+                return Math.sin(Math.toRadians(this.atome.interpret(context)));
+            case COS:
+                return Math.cos(Math.toRadians(this.atome.interpret(context)));
+            case TAN:
+                return Math.tan(Math.toRadians(this.atome.interpret(context)));
+            default:
+                return this.atome.interpret(context);
+        }
+    }
+}
